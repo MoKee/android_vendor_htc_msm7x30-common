4a4d35ca2735443521bee0c3e17d8ed418e47af0
Paul Bouman <boumanpaul@gmail.com>
2013-12-15 11:51:36 +0100

    Update libs from AU_LINUX_ANDROID_JB_VANILLA_04.02.02.060.053

--- qcom-vendor-blobs.mk
+++ qcom-vendor-blobs.mk
@@ -39,13 +39,14 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/proprietary/system/lib/egl/egl.cfg:/system/lib/egl/egl.cfg \
     vendor/qcom/proprietary/system/lib/egl/eglsubAndroid.so:/system/lib/egl/eglsubAndroid.so \
     vendor/qcom/proprietary/system/lib/egl/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \
-    vendor/qcom/proprietary/system/lib/egl/libGLES_android.so:/system/lib/egl/libGLES_android.so \
     vendor/qcom/proprietary/system/lib/egl/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \
     vendor/qcom/proprietary/system/lib/egl/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \
-    vendor/qcom/proprietary/system/lib/egl/libGLESv2S3D_adreno200.so:/system/lib/egl/libGLESv2S3D_adreno200.so \
+    vendor/qcom/proprietary/system/lib/egl/libplayback_adreno200.so:/system/lib/egl/libplayback_adreno200.so \
     vendor/qcom/proprietary/system/lib/egl/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so \
     vendor/qcom/proprietary/system/lib/libC2D2.so:/system/lib/libC2D2.so \
     vendor/qcom/proprietary/system/lib/libc2d2_z180.so:/system/lib/libc2d2_z180.so \
     vendor/qcom/proprietary/system/lib/libgsl.so:/system/lib/libgsl.so \
+    vendor/qcom/proprietary/system/lib/libllvm-arm.so:/system/lib/libllvm-arm.so \
+    vendor/qcom/proprietary/system/lib/libOpenCL.so:/system/lib/libOpenCL.so \
     vendor/qcom/proprietary/system/lib/libOpenVG.so:/system/lib/libOpenVG.so \
     vendor/qcom/proprietary/system/lib/libsc-a2xx.so:/system/lib/libsc-a2xx.so
